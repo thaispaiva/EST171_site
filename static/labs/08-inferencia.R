b23e883855bc904e7949ad01807b78113a44ed28
Thais <thaisvip@gmail.com>
2018-04-05 17:33:25 -0300

    working on slides 08

--- static/labs/08-inferencia.R
+++ static/labs/08-inferencia.R
@@ -1,3 +1,22 @@
 ## EST171 - Métodos Computacionais para Análise de Risco
 ## Código da aula 08 - Inferência
 
+## Pacotes
+# install.packages("fitdistrplus")  # instalar esse primeiro
+# install.packages("CASdatasets", repos = "http://cas.uqam.ca/pub/R/", type="source")
+require(CASdatasets)
+require(fitdistrplus)
+
+## dados seguro contra terceiros Itália
+data("itamtplcost")
+
+x = itamtplcost$UltimateCost/10^6
+summary(x)
+## EMV
+fgamEMV = fitdist(x, "gamma", method="mle")
+fgamEMV
+
+summary(fgamEMV)
+
+plot(fgamEMV)
+
